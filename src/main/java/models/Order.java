ffe73e1c08e64f990b73fd90a5a0294e71f04805
semenyutenko <semenyutenko@gmail.com>
2019-07-24 19:12:52 +0300

    added: getters, setters and constructors to Order

--- src/main/java/models/Order.java
+++ src/main/java/models/Order.java
@@ -1,4 +1,65 @@
 package models;
 
+import java.util.Date;
+
 public class Order {
+
+    private Date orderDate;
+    private Date compliteDate;
+    private Client client;
+    private Product product;
+    private double amount = 0;
+    private String comment = "";
+
+    public Order(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Date getCompliteDate() {
+        return compliteDate;
+    }
+
+    public void setCompliteDate(Date compliteDate) {
+        this.compliteDate = compliteDate;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
