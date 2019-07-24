6bc6361bbbaf699986c1d246cf50c52a616dfd0e
semenyutenko <semenyutenko@gmail.com>
2019-07-24 15:47:28 +0300

    added: DSNullPointedException

---
+++ src/main/java/exceptions/DSNullPointedException.java
@@ -0,0 +1,15 @@
+package exceptions;
+
+public class DSNullPointedException extends DSexception{
+
+    public String message;
+
+    public DSNullPointedException(String message){
+        this.message = message
+    }
+
+    public String getMessage(){
+        return message;
+    }
+
+}
--- src/main/java/models/Product.java
+++ src/main/java/models/Product.java
@@ -2,76 +2,30 @@ package models;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Product {
-    private static int commonId;
 
-    private final int productId;
     private String title;
     private Category category = Category.DRY_CURED;
     private double price = 0;
     private int amount = 0;
     private String description = "";
     private List<File> images = new ArrayList<File>();
+    private Date prodauctionDate;
 
     public Product(String title) {
-        commonId = getCommonId();
-        this.productId = commonId++;
         this.title = title;
     }
 
     public String getTitle() {
+        if (title == null) throw
         return title;
     }
 
     public void setTitle(String title) {
         this.title = title;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public List<File> getImages() {
-        return images;
-    }
-
-    public void setImages(List<File> images) {
-        this.images = images;
-    }
-
-    private static int getCommonId() {
-        return 0;
-        //TODO написать геттер на статический общий id для продукта;
     }
 
     public enum Category{
