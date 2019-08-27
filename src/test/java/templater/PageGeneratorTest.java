0a01342d8c8be7afa2b1a80a9a113ec007d10750
semenyutenko <semenyutenko@gmail.com>
2019-08-27 12:08:21 +0300

    have started to make tests

---
+++ src/test/java/templater/PageGeneratorTest.java
@@ -0,0 +1,16 @@
+package templater;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PageGeneratorTest {
+
+    @Test
+    public void instance() {
+    }
+
+    @Test
+    public void getPage() {
+    }
+}
